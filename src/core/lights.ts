2f91f31a981d4539ecd0fb95a8b9e378f5c64b42
Swarup <suryawanshiswarup@gmail.com>
2023-01-26 20:33:14 +0530

    move directional light folder

--- src/core/lights.ts
+++ src/core/lights.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { gui } from './gui'
 // Lights
 export const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 
@@ -8,3 +9,19 @@ directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.normalBias = 0.05
 directionalLight.position.set(0.25, 2, 2.25)
+
+const DirectionalLightFolder = gui.addFolder({
+    title: 'Directional Light',
+  })
+  
+  Object.keys(directionalLight.position).forEach(key => {
+    DirectionalLightFolder.addInput(
+      directionalLight.position,
+      key as keyof THREE.Vector3,
+      {
+        min: -100,
+        max: 100,
+        step: 1,
+      },
+    )
+  })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import * as THREE from 'three'
 import { scene } from './core/renderer'
-import { gui } from './core/gui'
 
 import './style.css'
 
 //lights
 import { ambientLight, directionalLight } from './core/lights'
+
 import plane from './objects/plane'
 import sphere from './objects/sphere'
 import loop from './core/animation'
@@ -13,23 +12,6 @@ import loop from './core/animation'
 scene.add(ambientLight)
 scene.add(directionalLight)
 scene.add(sphere)
-
-const DirectionalLightFolder = gui.addFolder({
-  title: 'Directional Light',
-})
-
-Object.keys(directionalLight.position).forEach(key => {
-  DirectionalLightFolder.addInput(
-    directionalLight.position,
-    key as keyof THREE.Vector3,
-    {
-      min: -100,
-      max: 100,
-      step: 1,
-    },
-  )
-})
-
 scene.add(plane)
 
 loop()
